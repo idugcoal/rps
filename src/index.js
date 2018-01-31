5dc0a8d33b3ae20356ad5e737f36b69cbb849c6f
Doug Cole <idugcoal@gmail.com>
2018-01-31 00:05:24 -0800

    add styles and function to the buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const helloWorld = () => {
     /*eslint-disable no-console */
-    let content = document.getElementById('content');
-    content.innerHTML = 'hello';
-    console.log(content);
+    document.getElementsByClassName('rock')[0].addEventListener('click', () => { alert('rock'); });
+    document.getElementsByClassName('paper')[0].addEventListener('click', () => { alert('paper'); });
+    document.getElementsByClassName('scissors')[0].addEventListener('click', () => { alert('scissors'); });
 
 };
 
