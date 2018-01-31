450be7df746f90d1c3a2f5a4c2d3bd2d7c2edf07
Doug Cole <idugcoal@gmail.com>
2018-01-31 00:46:24 -0800

    adds functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
 const helloWorld = () => {
     /*eslint-disable no-console */
-    document.getElementsByClassName('rock')[0].addEventListener('click', () => { alert('rock'); });
-    document.getElementsByClassName('paper')[0].addEventListener('click', () => { alert('paper'); });
-    document.getElementsByClassName('scissors')[0].addEventListener('click', () => { alert('scissors'); });
+    document.getElementsByClassName('rock')[0].addEventListener('click', () => { handleClick('rock'); });
+    document.getElementsByClassName('paper')[0].addEventListener('click', () => { handleClick('paper'); });
+    document.getElementsByClassName('scissors')[0].addEventListener('click', () => { handleClick('scissors'); });
+};
+
+const handleClick = (userChoice) => {
+
+    let computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+        computerChoice = 'rock';
+    } else if (computerChoice <= 0.67) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+
+    let content = document.getElementsByClassName('content')[0];
+    let result = compare(userChoice, computerChoice);
+    content.innerHTML = `You chose ${userChoice}. The computer chose ${computerChoice}. ${result}`
 
 };
+
+const compare = (choice1, choice2) => {
+    if (choice1 === choice2) {
+        return "It's a tie!";
+    }
+    if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+        // rock wins
+            return "You win!";
+        } else {
+        // paper wins
+            return "You lose! Try again.";
+        }
+    }
+    if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            // paper wins
+            return "You win!";
+        } else {
+            // scissors wins
+            return "You lose! Try again.";
+        }
+    }
+    if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            // rock wins
+            return "You lose! Try again.";
+        } else {
+            // scissors wins
+            return "You win!";
+        }
+    }
+};
+
 
 helloWorld();
