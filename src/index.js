760abbf742efd434991556a6b947af9d3cabffd6
Doug Cole <idugcoal@gmail.com>
2018-01-31 21:23:07 -0800

    refactor adding incrementCounter and writeOutput

--- src/index.js
+++ src/index.js
@@ -21,6 +21,27 @@ const handleReset = () => {
     counter.innerHTML = '';
 };
 
+const incrementCounter = (counterType) => {
+    let counter = document.getElementsByClassName('counter')[0];
+    let counterValue = parseInt(counter.getAttribute(counterType));
+    counter.setAttribute(counterType, ++counterValue);
+};
+
+const writeOutput = ({ playerNames, userChoice, computerChoice, result }) => {
+
+    let counter = document.getElementsByClassName('counter')[0];
+    let content = document.getElementsByClassName('content')[0];
+    let winner = document.getElementsByClassName('winner')[0];
+
+    let totalTies = parseInt(counter.getAttribute('data-ties'));
+    let p1Wins = parseInt(counter.getAttribute('data-p1-wins'));
+    let p2Wins = parseInt(counter.getAttribute('data-p2-wins'));
+
+    content.innerHTML = `${playerNames[0]} chose ${userChoice}. ${playerNames[1]} chose ${computerChoice}.`;
+    winner.innerHTML = `Winner: ${result}`;
+    counter.innerHTML = `<p>Player 1 wins: ${p1Wins} </p><p>Player 2 wins: ${p2Wins}</p><p>Ties: ${totalTies}</p>`;
+};
+
 const handleClick = (userChoice) => {
 
     let playerNames = ['You', 'The computer'];
@@ -31,30 +52,21 @@ const handleClick = (userChoice) => {
     }
 
     let computerChoice = getRandomChoice();
-    let counter = document.getElementsByClassName('counter')[0];
-    let content = document.getElementsByClassName('content')[0];
-    let winner = document.getElementsByClassName('winner')[0];
-    let totalTies = parseInt(counter.getAttribute('data-ties'));
-    let p1Wins = parseInt(counter.getAttribute('data-p1-wins'));
-    let p2Wins = parseInt(counter.getAttribute('data-p2-wins'));
 
     let result = compare(userChoice, computerChoice);
     if (result === 'tie') {
         result = 'Tie';
-        counter.setAttribute('data-ties', ++totalTies);
+        incrementCounter('data-ties');
     } else {
         if (result) {
             result = playerNames[0];
-            counter.setAttribute('data-p1-wins', ++p1Wins);
+            incrementCounter('data-p1-wins');
         } else {
             result = playerNames[1];
-            counter.setAttribute('data-p2-wins', ++p2Wins);
+            incrementCounter('data-p2-wins');
         }
     }
-    content.innerHTML = `${playerNames[0]} chose ${userChoice}. ${playerNames[1]} chose ${computerChoice}.`;
-    winner.innerHTML = `Winner: ${result}`;
-    counter.innerHTML = `<p>Player 1 wins: ${p1Wins} </p><p>Player 2 wins: ${p2Wins}</p><p>Ties: ${totalTies}</p>`;
-
+    writeOutput({ playerNames, userChoice, computerChoice, result });
 };
 
 const getRandomChoice = () => {
